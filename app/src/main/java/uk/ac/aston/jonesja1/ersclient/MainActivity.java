7fa2318997db9ab62717247b7a873a21f93346be
James Jones <jms.a.jns@gmail.com>
2017-05-10 12:59:08 +0100

    inital commit

---
+++ app/src/main/java/uk/ac/aston/jonesja1/ersclient/MainActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.aston.jonesja1.ersclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
